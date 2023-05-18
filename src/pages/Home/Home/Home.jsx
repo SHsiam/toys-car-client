9825c377db7c3f6674997cb133587b63c2afb00a
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-19 03:28:44 +0600

    banner done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,49 @@
-
+import { Carousel } from "react-bootstrap";
+import car1 from '../../../assets/cars/car1.jpg'
+import car2 from '../../../assets/cars/car2.jpg'
+import car3 from '../../../assets/cars/car3.jpg'
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home page</h2>
-        </div>
+        <Carousel >
+      <Carousel.Item >
+        <img
+          className="d-block w-100 "
+          src={car1}
+          alt="First slide"
+        />
+        <Carousel.Caption >
+          <h3>Experience opulence and style with this golden luxury car!</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={car2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption >
+          <h3>Embrace timeless beauty with this stunning retro car!</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={car3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>
+          Make a statement with this timeless classic black car!
+          </h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+ 
+    </div>
     );
 };
 
