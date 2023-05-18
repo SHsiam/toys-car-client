7e1c17ee5dab609b4d2e72d608104bb28cda2023
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-19 05:12:14 +0600

    extra section also done

--- src/pages/Home/Extrasection/Extrasection.jsx
+++ src/pages/Home/Extrasection/Extrasection.jsx
@@ -1,6 +1,10 @@
 import brand1 from '../../../assets/cars/brands/brand-1.jpg'
 import brand2 from '../../../assets/cars/brands/brand-2.jpg'
 import brand3 from '../../../assets/cars/brands/brand-3.jpg'
+import partner1 from '../../../assets/cars/pratners/partner-1.png'
+import partner2 from '../../../assets/cars/pratners/partner-2-removebg-preview.png'
+import partner3 from '../../../assets/cars/pratners/partner-3-removebg-preview.png'
+import partner4 from '../../../assets/cars/pratners/partner-4-removebg-preview.png'
 
 const Extrasection = () => {
     return (
@@ -20,9 +24,26 @@ const Extrasection = () => {
                     <img src={brand3} className="card-img-top w-100 h-100" alt="brand-3" />
                 </div>
             </div>
-             {/* section 2 brand name */}
-        </div>
-    );
+            {/* section 2 brand name */}
+            <div className="container text-center">
+            <h2 className='text-center text-6xl mb-4 pt-4'>Our Partners</h2>
+                <div className="row">
+                    <div className="col">
+                        <img src={partner1} alt="partner-1" />
+                    </div>
+                    <div className="col">
+                    <img src={partner2} alt="partner-2" />
+                    </div>
+                    <div className="col">
+                    <img src={partner3} alt="partner-3" />
+                    </div>
+                    <div className="col">
+                    <img src={partner4} alt="partner-4" />
+                    </div>
+                </div>
+                </div>
+            </div>
+            );
 };
 
-export default Extrasection;
+            export default Extrasection;
