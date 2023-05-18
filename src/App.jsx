8ff17a896b01dcf78074a1c57c1eae25c40e4855
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-18 22:27:10 +0600

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +10,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <Link to="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </Link>
+        <Link to="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </Link>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Toys Car</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
