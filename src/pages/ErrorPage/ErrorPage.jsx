8d60c2ad14636d67ecc521ae545e8fe6379d3231
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-20 11:33:38 +0600

    error page design is done

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,15 @@
+
+import error from '../../assets/error.png'
+
+const ErrorPage = () => {
+    return (
+            <div>
+                <img src={error}/>
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+
+    );
+};
+
+export default ErrorPage;
