994913ece9a7656568eca39ce3322fb7a20adc26
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-21 19:48:49 +0600

    off road car also done

--- src/pages/Home/Shopcategory/Shopcategory.jsx
+++ src/pages/Home/Shopcategory/Shopcategory.jsx
@@ -6,6 +6,8 @@ import luxary from '../../../assets/cars/luxury.jpg'
 import luxary1 from '../../../assets/cars/luxury-1.jpg'
 import car3 from '../../../assets/cars/car2.jpg'
 import car4 from '../../../assets/cars/car4.jpg'
+import car5 from '../../../assets/cars/car3.jpg'
+import car6 from '../../../assets/cars/offroad.jpg'
 
 const Shopcategory = () => {
     return (
@@ -104,7 +106,30 @@ const Shopcategory = () => {
 </div>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
+      <img src={car5} className="card-img-top img-thumbnail" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Xtreme Offroader</h5>
+        <p className="card-text">Price: $200</p>
+        <p className="card-text">Rating: 5</p>
+        <button type="button" className="btn btn-primary">View Details</button>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src={car6} className="card-img-top img-thumbnail" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Mountain Beast</h5>
+        <p className="card-text">Price: $300</p>
+        <p className="card-text">Rating: 5</p>
+        <button type="button" className="btn btn-primary">View Details</button>
+      </div>
+    </div>
+  </div>
+</div>
     </TabPanel>
   </Tabs>
   </div>
